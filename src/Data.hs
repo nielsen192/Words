73fa7b745b5602819ce2d25b6ca99552d8b38df9
nielsen192 <nielsen192@gmail.com>
2017-11-01 15:41:25 +0100

    updated files

---
+++ src/Data.hs
@@ -0,0 +1,44 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  Data
+-- Copyright   :  BSD
+-- License     :  BSD3
+--
+-- Maintainer  :  nielsen192@gmail.com
+-- Stability   :
+-- Portability :
+--
+-- |
+--
+-----------------------------------------------------------------------------
+
+module Data ( grid
+            , languages
+) where
+
+grid = [ "__C________R___"
+       , "__SI________U__"
+       , "__HASKELL____B_"
+       , "__A__A_____S__Y"
+       , "__R___B___C____"
+       , "__PHP____H_____"
+       , "____S_LREP_____"
+       , "____I__M_Y__L__"
+       , "____L_E__T_O___"
+       , "_________HB____"
+       , "_________O_____"
+       , "________CN_____"
+       ]
+
+languages = [ "BASIC"
+            , "COBOL"
+            , "CSHARP"
+            , "HASKELL"
+            , "LISP"
+            , "PERL"
+            , "PHP"
+            , "PYTHON"
+            , "RUBY"
+            , "SCHEME"
+            ]
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,14 +1,15 @@
 module Lib
-    (grid
-    ,languages
-    ,formatGrid
+    (
+     formatGrid
     ,outputGrid
     ,findWords
     ,findWord
     ,findWordInLine
+    ,skew
     ) where
 
-import Data.List (isInfixOf)
+
+import Data.List (isInfixOf, transpose)
 import Data.Maybe (catMaybes)
 
 type Grid = [String]
@@ -19,9 +20,27 @@ outputGrid grid = putStrLn (formatGrid grid)
 formatGrid :: Grid -> String
 formatGrid = unlines
 
+getLines :: Grid -> [String]
+getLines grid =
+    let horizontal = grid
+        vertical = transpose grid
+        diagonal1 = diagonalize grid
+        diagonal2 = diagonalize (map reverse grid)
+        lines = horizontal ++ vertical ++ diagonal1 ++ diagonal2
+    in lines ++ (map reverse lines)
+
+diagonalize :: Grid -> Grid
+-- diagonalize grid = transpose (skew grid)
+diagonalize = transpose . skew
+
+skew :: Grid -> Grid
+skew [] = []
+skew (l:ls) = l : skew (map indent ls)
+    where indent line = '_' : line
+
 findWord :: Grid -> String -> Maybe String
 findWord grid word =
-    let lines = grid ++ (map reverse grid)
+    let lines = getLines grid
         found = or $ map (findWordInLine word) lines
     in if found then Just word else Nothing
 
@@ -35,29 +54,3 @@ findWords grid words =
 findWordInLine :: String -> String -> Bool
 -- findWordInLine word line = word `isInfixOf` line
 findWordInLine = isInfixOf
-
-grid = [ "__C________R___"
-       , "__SI________U__"
-       , "__HASKELL____B_"
-       , "__A__A_____S__Y"
-       , "__R___B___C____"
-       , "__PHP____H_____"
-       , "____S_LREP_____"
-       , "____I__M_Y__L__"
-       , "____L_E__T_O___"
-       , "_________HB____"
-       , "_________O_____"
-       , "________CN_____"
-       ]
-
-languages = [ "BASIC"
-            , "COBOL"
-            , "CSHARP"
-            , "HASKELL"
-            , "LISP"
-            , "PERL"
-            , "PHP"
-            , "PYTHON"
-            , "RUBY"
-            , "SCHEME"
-            ]
